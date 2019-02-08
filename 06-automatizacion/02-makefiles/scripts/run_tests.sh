372cb3bf252a2123b91282d3c7b5663fe524d3b5
Your Name <you@example.com>
2019-02-07 21:07:38 -0800

    changes

--- 06-automatizacion/02-makefiles/scripts/run_tests.sh
+++ 06-automatizacion/02-makefiles/scripts/run_tests.sh
@@ -1,8 +1,8 @@
-docker run --name $APP_NAME -d -p $PORT:$PORT $APP_NAME:$IMAGE_VERSION 
+docker run --name $APP_NAME -d -p $HOST_PORT:$CONTAINER_PORT $APP_NAME:$IMAGE_VERSION 
 
 sleep ${TEST_GRACE_PERIOD}
 
-curl localhost:$PORT
+curl localhost:$HOST_PORT
 test_status=$?
 
 docker rm -f ${APP_NAME}
